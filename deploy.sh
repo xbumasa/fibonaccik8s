a7491613109c8da94c588a68e1e5b7a50e6a5f1e
Semion Shulman <shulman.dev@gmail.com>
2021-02-05 16:57:28 +0200

    Travis config update & deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t xbumasa/fibonaccik8s-ui:latest -t xbumasa/fibonaccik8s-ui:$GIT_SHA ./ui
+docker build -t xbumasa/fibonaccik8s-api:latest -t xbumasa/fibonaccik8s-api:$GIT_SHA ./api
+docker build -t xbumasa/fibonaccik8s-worker:latest -t xbumasa/fibonaccik8s-worker:$GIT_SHA ./worker
+docker push xbumasa/fibonaccik8s-ui:latest
+docker push xbumasa/fibonaccik8s-api:latest
+docker push xbumasa/fibonaccik8s-worker:latest
+docker push xbumasa/fibonaccik8s-ui:$GIT_SHA
+docker push xbumasa/fibonaccik8s-api:$GIT_SHA
+docker push xbumasa/fibonaccik8s-worker:$GIT_SHA
+kubectl apply -f k8s
+kubectl set image deployments/api-deployment api=xbumasa/fibonaccik8s-api:$GIT_SHA
+kubectl set image deployments/ui-deployment ui=xbumasa/fibonaccik8s-ui:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=xbumasa/fibonaccik8s-worker:$GIT_SHA
